0d600ca9eaa2014f686607ae9c63cd7ccff9e8f7
yared-ayele-debela <yaredayele67@gmail.com>
2025-12-16 10:09:12 +0000

    feat: Initialize Laravel project scaffold
    Initial commit from a fresh Laravel installation, including core files, Composer setup, and basic bootstrap.

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::resource('questions',\App\Http\Controllers\QuestinsController::class)->except('show');
+Route::get('/questions/{slug}',[\App\Http\Controllers\QuestinsController::class,'show'])->name('questions.show');
+require __DIR__.'/auth.php';
